e7ad785651d674a1723b3f0c376725559ede4392
Aarini123 <77624143+Aarini123@users.noreply.github.com>
2021-06-30 17:27:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
 function preload(){
 song=loadSound("music.mp3");
 }
@@ -8,6 +13,22 @@ function setup(){
     canvas.position(380,250);
     video= createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotposes);
+}
+
+function gotposes(results){
+  if (results.length>0){
+      console.log(results);
+      leftWristX=results[0].pose.leftWrist.x;
+      leftWristY=results[0].pose.leftWrist.y;
+      rightWristX=results[0].pose.rightWrist.x;
+      rightWristY=results[0].pose.rightWrist.y;
+  } 
+}
+
+function modelLoaded(){
+    console.log("M0dE!L0aDeD!");
 }
 
 function draw(){
@@ -16,4 +37,6 @@ function draw(){
 
 function play_music(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
