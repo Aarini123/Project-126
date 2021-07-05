62822c8461807a00b2117482ed0033495643d858
Aarini123 <77624143+Aarini123@users.noreply.github.com>
2021-07-05 16:50:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 song="";
+song1="";
+song_status="";
+song1_status="";
 leftWristX=0;
 leftWristY=0;
 rightWristX=0;
@@ -8,6 +11,7 @@ RightwristScore=0;
 
 function preload(){
 song=loadSound("music.mp3");
+song1=loadSound("music2.mp3");
 }
 
 function setup(){
@@ -26,6 +30,7 @@ function gotposes(results){
       rightWristX=results[0].pose.rightWrist.x;
       rightWristY=results[0].pose.rightWrist.y;
       LeftwristScore=results[0].pose.keypoints[9].score;
+      RightwristScore=results[0].pose.keypoints[10].score;
 
   } 
 }
@@ -39,21 +44,28 @@ function draw(){
     image(video,0,0,600,400);
     fill("#06003b");
     stroke("#06003b");
+    song_status=song.isPlaying();
+    song1_status=song1.isPlaying();
     if(LeftwristScore>0.2){
         circle(leftWristX,leftWristY,20);
-
-        Yvalue=Number(leftWristY);
-        Yvaluenum=floor(Yvalue);
-        volume=Yvaluenum/400;
-        document.getElementById("btn btn-info H3").innerHTML="Harry Potter Theme Song";
-    }
-    if(RightwristScore>0.2){
-    Xvalue=Number(leftWristX);
-    Xvaluenum=floor(Xvalue);
-    volume2=Xvaluenum/400;
-    document.getElementById("btn btn-info H3").innerHTML="Peter Pan";
-    song.play("Groove Delgiht & Samantha Machado-Peter Pan(Official Lyric Video").mp3;
+song.stops();
+if(song1_status==false){
+    song1.play();
+    document.getElementById("songName").innerHTML="Peter Pan song";
 }
+   
+}
+if(RightwristScore>0.2){
+    circle(rightWristX,rightWristY,20);
+song1.stops();
+if(song_status==false){
+song.play();
+document.getElementById("songName").innerHTML="Harry Potter Theme song";
+}
+
+}
+
+
 }
 
 function play_music(){
